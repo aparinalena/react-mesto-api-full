61a9afc549c5c8fce5a6e8db6f8009edfb4355bf
Alena Aparina <yelenaaparina@yandex.ru>
2022-01-30 16:35:30 +0300

    CORS

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,18 +1,21 @@
 const jwt = require('jsonwebtoken');
 const AuthError = require('../errors/AuthError');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
 module.exports = (req, res, next) => {
-  const { NODE_ENV, JWT_SECRET } = process.env;
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new AuthError('Необходима авторизоваться');
+  }
+  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(
-      req.cookies.token,
-      NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
-    );
-  } catch (err) {
-    throw new AuthError('Необходимо авторизоваться');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+  } catch (e) {
+    throw new AuthError('Необходима авторизоваться');
   }
   req.user = payload;
-  next();
+
+  return next();
 };
