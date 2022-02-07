e04bb9a975facfd880b79d67a2962380793e119e
Alena Aparina <yelenaaparina@yandex.ru>
2022-02-07 22:37:17 +0300

    fix: token

--- frontend/src/components/EditAvatarPopup.js
+++ frontend/src/components/EditAvatarPopup.js
@@ -11,9 +11,7 @@ function EditAvatarPopup({ isOpen, onClose, onUpdateAvatar }) {
   function handleSubmit(e) {
     e.preventDefault();
 
-    onUpdateAvatar({
-      avatar: avatarRef.current.value,
-    });
+    onUpdateAvatar(avatarRef.current.value);
   }
 
   return (
